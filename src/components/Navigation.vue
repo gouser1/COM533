9fe6b8b441584cc0bd55e43d87136b4d3699327a
BuildTools <unconfigured@null.spigotmc.org>
2019-04-13 22:11:48 +0100

    Fixed Business.vue scripting

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,48 +1,32 @@
 <template>
-    <span>
-        <!--Span to allow <v-navigation-drawer> and <v-toolbar> roots together !-->
-        <v-navigation-drawer
-            app
-            color="#0D1514"
-            v-model="drawer"
-            disable-resize-watcher
-        >
-            <!--Vuetify navigation drawer for smaller screen sizes !-->
-            <v-list>
-                <template v-for="(item, index) in items">
-                    <v-list-tile :key="index">
-                        <v-list-tile-content>
-                            {{ item.title }}
-                        </v-list-tile-content>
-                    </v-list-tile>
-                    <v-divider :key="`divider-${index}`"></v-divider>
-                </template>
-            </v-list>
-        </v-navigation-drawer>
-        <v-toolbar app color="#0D1514" dark>
-            <v-toolbar-side-icon
-                class="hidden-md-and-up"
-                @click="drawer = !drawer"
-            ></v-toolbar-side-icon>
-            <v-spacer class="hidden-md-and-up"></v-spacer>
-            <router-link to="/">
-                <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
-            </router-link>
-            <!-- Larger screen navigation bar! -->
-            <v-btn flat class="hidden-sm-and-down" to="/popular">Popular</v-btn>
-            <v-btn flat class="hidden-sm-and-down" to="/business"
-                >Business</v-btn
-            >
-            <v-btn flat class="hidden-sm-and-down" to="/entertainment"
-                >Entertainment</v-btn
-            >
-            <v-btn flat class="hidden-sm-and-down" to="/technology"
-                >Technology</v-btn
-            >
-            <v-btn flat class="hidden-sm-and-down" to="/science">Science</v-btn>
-            <v-btn flat class="hidden-sm-and-down" to="/sport">Sport</v-btn>
-        </v-toolbar>
-    </span>
+  <span>
+    <!--Span to allow <v-navigation-drawer> and <v-toolbar> roots together !-->
+    <v-navigation-drawer app color="#0D1514" v-model="drawer" disable-resize-watcher>
+      <!--nav drawer for smaller screen sizes-->
+      <v-list>
+        <template v-for="(item, index) in items">
+          <v-list-tile :key="index">
+            <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+          </v-list-tile>
+          <v-divider :key="`divider-${index}`"></v-divider>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app color="#0D1514" dark>
+      <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer class="hidden-md-and-up"></v-spacer>
+      <router-link to="/">
+        <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
+      </router-link>
+      <!-- Larger screen nav bar -->
+      <v-btn flat class="hidden-sm-and-down" to="/popular">Popular</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/business">Business</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/entertainment">Entertainment</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/technology">Technology</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/science">Science</v-btn>
+      <v-btn flat class="hidden-sm-and-down" to="/sport">Sport</v-btn>
+    </v-toolbar>
+  </span>
 </template>
 
 <script>
--- src/views/Business.vue
+++ src/views/Business.vue
@@ -1,86 +1,65 @@
 <template>
-    <v-container grid-list-lg>
-        <v-layout row>
-            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5"
-                >{{ $route.meta.title }} News</v-flex
-            >
-        </v-layout>
-        <v-layout row wrap v-for="article in articles" :key="article.id">
-            <v-flex xs12 class="text-xs-center font-weight-black my-5">
-                <v-card data-aos="fade-up">
-                    <v-responsive>
-                        <img
-                            v-bind:src="article.urlToImage"
-                            height="400"
-                            class="my-3"
-                        />
-                        <v-container fill-height fluid>
-                            <v-layout fill-height>
-                                <v-flex xs12 align-end flexbox>
-                                    <v-card-text class="title">
-                                        {{ article.title }}
-                                    </v-card-text>
-                                    <v-spacer></v-spacer>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-responsive>
-                    <v-card-text>{{ article.content }}</v-card-text>
-                    <v-card-actions>
-                        <v-dialog width="500" lazy>
-                            <template v-slot:activator="{ on }">
-                                <v-btn outline block color="green" v-on="on"
-                                    >Article Snippet</v-btn
-                                >
-                            </template>
+  <v-container grid-list-lg>
+    <v-layout row>
+      <v-flex
+        xs12
+        class="text-xs-center display-1 font-weight-black my-5"
+      >{{ $route.meta.title }} News</v-flex>
+    </v-layout>
+    <!-- For loop to assign each array item with the id 'article' !-->
+    <v-layout row wrap v-for="article in articles" :key="article.id">
+      <v-flex xs12 class="text-xs-center font-weight-black my-5">
+        <v-card data-aos="fade-up">
+          <v-responsive>
+            <img v-bind:src="article.urlToImage" height="400" class="my-3">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <v-card-text class="title">{{ article.title }}</v-card-text>
+                  <v-spacer></v-spacer>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-responsive>
+          <v-card-text>{{ article.content }}</v-card-text>
+          <v-card-actions>
+            <v-dialog width="500" lazy>
+              <template v-slot:activator="{ on }">
+                <v-btn outline block color="green" v-on="on">Article Snippet</v-btn>
+              </template>
 
-                            <v-card-title
-                                class="headline grey lighten-2"
-                                primary-title
-                                >Article Snippet</v-card-title
-                            >
+              <v-card-title class="headline grey lighten-2" primary-title>Article Snippet</v-card-title>
 
-                            <v-card-text class="grey lighten-2">
-                                {{ article.content }}
-                            </v-card-text>
-                        </v-dialog>
-                        <v-btn
-                            outline
-                            block
-                            color="blue"
-                            :href="article.url"
-                            target="_blank"
-                            >Read Full Article</v-btn
-                        >
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+              <v-card-text class="grey lighten-2">{{ article.content }}</v-card-text>
+            </v-dialog>
+            <v-btn outline block color="blue" :href="article.url" target="_blank">Read Full Article</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios'; //Import axios library
 export default {
-    data: function() {
+    created() {
+        axios // Axios get request for NewsAPI resource in JSON format
+            .get(
+                'https://newsapi.org/v2/top-headlines?country=gb&category=business&pageSize=20&apiKey=aaffa4aad4294bb0af6a1b3f7248e1b3'
+            )
+            .then(response => {
+                this.articles = response.data.articles; // Store the articles from NewsAPI in articles array
+            });
+    },
+    data() {
         return {
-            articles: []
+            active: null,
+            category: '',
+            articles: [] // Array containing stored articles
         };
     },
-    mounted: function() {
-        var that = this;
-        var url =
-            'https://newsapi.org/v2/top-headlines?' +
-            'country=us&' +
-            'apiKey=aaffa4aad4294bb0af6a1b3f7248e1b3';
-        var req = new Request(url);
-        fetch(req)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                that.articles = data.articles;
-            });
-    }
+    methods: {}
 };
 </script>
 
